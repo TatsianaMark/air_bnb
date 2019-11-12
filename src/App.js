c2f63a719f0e031ed9c120a2ab6a317f66f133c3
TatsianaMark <tanya030890@mail.ru>
2019-11-12 21:23:47 +0300

    added form without styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-promo borders" >
+      <form action="/" method="GET" className="main-search borders">
+       <h2>Бронируйте уникальное жилье и Впечатления</h2>
+        <div className="field">
+        <label htmlFor ="Where">Where</label>
+        <input type="text" id="Where"/>
+        </div>
+        <div className="field-group">
+          <div className="field">
+            <label htmlFor ="from"></label>
+            <input type="date" id="from"/>
+          </div>
+          <div className="field">
+            <label htmlFor ="to"></label>
+            <input type="date" id="to"/>
+          </div>
+        </div>
+        <div className="field-group">
+          <div className="field">
+            <label htmlFor ="from"></label>
+            <select id="adults">
+                <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+
+            </select>
+          </div>
+          <div className="field">
+            <label htmlFor ="to"></label>
+            <select id="child">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            </select>
+          </div>
+          <button className="button">Search</button>
+        </div>
+      </form>
     </div>
+    </div>
+
+
   );
 }
 
